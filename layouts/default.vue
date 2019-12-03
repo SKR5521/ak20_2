495c844b9dbff13eb808bcb2b35f70a1814d3f2e
SKR5521 <48979021+SKR5521@users.noreply.github.com>
2019-12-03 15:54:01 +0530

    new one

---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app class="bdy">
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.bdy {
+  margin: 0;
+  padding: 0;
+  background-color: $bg !important;
+}
+
+.tb {
+  @extend .bdy;
+
+  color: yellow;
+}
+</style>
